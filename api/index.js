6b8ce9beb43dd1fee2752b7a49787430e1886a92
Artur Iwasaki Lourenço <hiroiwl@gmail.com>
2025-06-04 19:43:59 -0300

    feat: conexão bd

--- api/index.js
+++ api/index.js
@@ -1,10 +1,16 @@
 import express from "express";
-import { Client } from "memjs";
 import { json } from "milliparsec";
+import "dotenv/config";
+import { MongoClient } from "mongodb";
+import { v4 as uuid } from "uuid";
 
 const app = express();
 app.use(json());
-const users = [];
+
+async function getDatabase() {
+  const client = new MongoClient(process.env.CONNECTION_STRING);
+  return client.db("auth");
+}
 
 const PORT = 3000;
 app.listen(PORT, () => {
@@ -15,32 +21,55 @@ app.get("/", (_, res) => {
   res.status(200).json({ message: "ta funcionando ʕ·͡ᴥ·ʔ" });
 });
 
+app.get("/users", async (req, res) => {
+  const db = await getDatabase();
+  const collection = db.collection("users");
+
+  const users = await collection.find().toArray();
+
+  res.status(200).json(
+    users.map((user) => ({
+      ...user,
+      password: undefined,
+    }))
+  );
+});
+
 app.post("/users", async (req, res) => {
   const { email, password, phone, nome } = req.body;
+  const db = await getDatabase();
 
-  if (users.find((x) => x.user === user)) {
-    res.status(400).json({ message: "Usuário já existe" });
+  const user = await db.collection("users").findOne({ email: email });
+  if (user) {
+    res.status(400).json({
+      message: "Usuário já cadastrado.",
+    });
   }
 
-  const user = {
-    id: users.length + 1,
+  const newUser = {
+    id: uuid(),
     password,
     phone,
     nome,
     email,
   };
 
-  users.push(user);
+  await db.collection("users").insertOne(newUser);
 
-  res.status(201).json({
-    ...user,
+  res.status(204).json({
+    ...newUser,
     password: undefined,
   });
 });
 
-app.post("/login", (req, res) => {
+app.post("/login", async (req, res) => {
   const { email, password } = req.body;
-  const user = users.find((x) => x.email === email && x.password === password);
+  const db = await getDatabase();
+  const user = await db.collection("users").findOne({
+    email: email,
+    password: password,
+  });
+
   if (!user) {
     res.status(400).json({
       message: "Usuário ou senha incorretos.",
