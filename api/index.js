6f31bf39bdfc8ee02263c2d5c475d9677d221b92
Artur Iwasaki Lourenço <hiroiwl@gmail.com>
2025-06-04 19:10:37 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,7 +1,6 @@
-import express from 'express'
-import { Client } from 'memjs';
-import { json } from 'milliparsec';
-
+import express from "express";
+import { Client } from "memjs";
+import { json } from "milliparsec";
 
 const app = express();
 app.use(json());
@@ -9,50 +8,47 @@ const users = [];
 
 const PORT = 3000;
 app.listen(PORT, () => {
-    console.log(`server running  on ${PORT} 🎉`);
+  console.log(`server running  on ${PORT} 🎉`);
 });
 
-app.get('/healthcheck', (_, res) => {
-    res
-        .status(200)
-        .json({ message: 'ta funcionando ʕ·͡ᴥ·ʔ' })
+app.get("/", (_, res) => {
+  res.status(200).json({ message: "ta funcionando ʕ·͡ᴥ·ʔ" });
 });
 
-app.post('/users', async (req, res) => {
-    const { email, password, phone, nome } = req.body;
+app.post("/users", async (req, res) => {
+  const { email, password, phone, nome } = req.body;
 
-    if (users.find(x => x.user === user)) {
-        res.status(400).json({ message: 'Usuário já existe' })
-    }
+  if (users.find((x) => x.user === user)) {
+    res.status(400).json({ message: "Usuário já existe" });
+  }
 
+  const user = {
+    id: users.length + 1,
+    password,
+    phone,
+    nome,
+    email,
+  };
 
-    const user = {
-        id: users.length + 1,
-        password,
-        phone,
-        nome,
-        email,
-    };
+  users.push(user);
 
-    users.push(user);
-
-    res.status(201).json({
-        ...user,
-        password: undefined,
-    })
+  res.status(201).json({
+    ...user,
+    password: undefined,
+  });
 });
 
-app.post('/login', (req, res) => {
-    const { email, password } = req.body;    
-    const user = users.find(x => x.email === email && x.password === password);
-    if (!user) {
-        res.status(400).json({
-            message: 'Usuário ou senha incorretos.'
-        })
-    }
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  const user = users.find((x) => x.email === email && x.password === password);
+  if (!user) {
+    res.status(400).json({
+      message: "Usuário ou senha incorretos.",
+    });
+  }
 
-    res.status(200).json({
-        ...user,
-        password: undefined
-    })
-})
+  res.status(200).json({
+    ...user,
+    password: undefined,
+  });
+});
